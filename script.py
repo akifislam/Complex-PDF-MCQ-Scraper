9ae3207fe003f91974b6e731ed51be8e591a0c18
Akif Islam <iamakifislam@gmail.com>
2022-08-09 14:08:06 +0600

    input on XLS

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import pdfplumber
 import csv
 import time
 PDF_PATH = '/Users/akifislam/PycharmProjects/PlumberTest/Resources/AS  Biology (9700)/2011/9700_s11_qp_11.pdf'
-PAGE_NUMBER = 4
+PAGE_NUMBER = 6
 
 SL_COLUMN_A = []  # For SL Input in Excels
 QUES_TEXT_COLUMN_B = [] # For Question in Excels
@@ -19,7 +19,7 @@ def updateQTABLEStatus(index,status):
 
 
 def countSerial():
-    count = 10 #Start of Page Count
+    count = 16 #Start of Page Count
     with pdfplumber.open(PDF_PATH) as pdf:
         page_all_texts = (pdf.pages[PAGE_NUMBER].extract_text())
         page_all_texts = page_all_texts
@@ -145,10 +145,15 @@ def collectOptions():
 
 
 def getAnswerTableLocation():
-    print(len(OPTIONS_COLUMN_I_J_K_L))
+    # print(len(OPTIONS_COLUMN_I_J_K_L))
     for i in range (0,len(OPTIONS_COLUMN_I_J_K_L)):
-        if(OPTIONS_COLUMN_I_J_K_L[i][1].count("  ")>=1):
-            HAS_ANY_ANSWER_TABLE.append(True)
+        if(OPTIONS_COLUMN_I_J_K_L[i][1].count("  "))>=1:
+            LEN = len(OPTIONS_COLUMN_I_J_K_L[i][1])
+            IND = OPTIONS_COLUMN_I_J_K_L[i][1].find("  ")
+            if(LEN-IND>2):
+                HAS_ANY_ANSWER_TABLE.append(True)
+            else:
+                HAS_ANY_ANSWER_TABLE.append(False)
         else:
             HAS_ANY_ANSWER_TABLE.append(False)
 
@@ -156,8 +161,8 @@ def getAnswerTableData():
     with pdfplumber.open(PDF_PATH) as pdf:
         # print(pdf.pages[PAGE_NUMBER].extract_tables())
         ALL_TABLE_DATA.append(pdf.pages[PAGE_NUMBER].extract_tables())
-    if(len(ALL_TABLE_DATA[0])!=len(OPTIONS_COLUMN_I_J_K_L)):
-            print("Q TABLE FOUND")
+    # if(len(ALL_TABLE_DATA[0])!=len(OPTIONS_COLUMN_I_J_K_L)):
+    #         # print("Q TABLE FOUND")
 
 
 
@@ -167,7 +172,7 @@ collectOptions()
 
 getAnswerTableLocation()
 getAnswerTableData()
-
+# print(OPTIONS_COLUMN_I_J_K_L)
 
 
 for i in range (0,len(ALL_TABLE_DATA[0])):
@@ -177,13 +182,14 @@ for i in range (0,len(ALL_TABLE_DATA[0])):
     for j in range (0,len(ALL_TABLE_DATA[0][i])):
         if ('A' in ALL_TABLE_DATA[0][i][j] or 'B' in ALL_TABLE_DATA[0][i][j] or 'C' in ALL_TABLE_DATA[0][i][j] or 'D' in ALL_TABLE_DATA[0][i][j]):
             AB_counter+=1
+        if 'A \nB \nC \nD' in ALL_TABLE_DATA[0][i][j]:
+            AB_counter=4
 
     if(AB_counter==4):
         ANSWER_TABLE_DATA.append(ALL_TABLE_DATA[0][i])
 
 
-print(HAS_ANY_ANSWER_TABLE)
-print(ANSWER_TABLE_DATA[0][2])
+
 newCSVfile = open('/Users/akifislam/Desktop/summary.csv', 'w')
 writer = csv.writer(newCSVfile)
 
@@ -191,6 +197,7 @@ writer = csv.writer(newCSVfile)
 # Adding Header
 writer.writerow(['SL.','Question Name*','Question Text*','Question Table <QTABLE>','Answer Format*','Answer Column header/s Level1*','Answer Column header/s Level2','Answer Row header/s Level2','Option A*','Option B*','Option C*','Option D*','Correct Answer*','Tags-Topic','Tag-Difficulty*','Tag-UniqueID*','General Feedback','Grade(Default =1)','Penalty (default = none)','Shuffle Answers (default = no)','Answer numbering (default = abcd..)'])
 # Adding Data
+
 for i in range (0,len(SL_COLUMN_A)):
     cur_seral = SL_COLUMN_A[i];
     cur_qus_name = PDF_PATH.split('/')[-1].split('.')[0] + "_" + str(i)
@@ -202,12 +209,17 @@ for i in range (0,len(SL_COLUMN_A)):
     cur_ans_col_header_l1 = ""
     cur_answer_format = ""
 
+    # print(ALL_TABLE_DATA)
+    # print(ANSWER_TABLE_DATA)
     # Answer Format
+    # print(HAS_ANY_ANSWER_TABLE)
+
     if(HAS_ANY_ANSWER_TABLE[i]==True):
         # Getting Table Format
         cur_answer_format = "With Table"
 
         # Check for L1 Headers
+
         if "A" not in ANSWER_TABLE_DATA[i][0]:
             for item in ANSWER_TABLE_DATA[i][0]:
                 if(str(item).isspace() or item=="," or item==None):
@@ -226,18 +238,28 @@ for i in range (0,len(SL_COLUMN_A)):
         cur_ans_col_header_l1=""
         cur_answer_format = "No Table"
 
+    # print(ALL_TABLE_DATA)
     #Filliing Up Options
     if(HAS_ANY_ANSWER_TABLE[i]==True):
+        # print("Table Found")
         for data in ANSWER_TABLE_DATA[i]:
             # print("data: ", data)
-            if("A" in data or "B" in data or "C" in data or "D" in data):
-                cur_options.append(data)
+            if("A" in data or "B" in data or "C" in data or "D" in 'A \nB \nC \nD' in data ):
+                cur_options = ["ANSWER TABLE DATA 1", "ANSWER TABLE DATA 2", "ANSWER TABLE DATA 3", "ANSWER TABLE DATA 4"]
     else:
         cur_options = [OPTIONS_COLUMN_I_J_K_L[i][0], OPTIONS_COLUMN_I_J_K_L[i][1], OPTIONS_COLUMN_I_J_K_L[i][2],OPTIONS_COLUMN_I_J_K_L[i][3]]
 
-
+    print(cur_options)
+    #Current Option Processing
+    # cur_options_processed = []
+    #
+    # for eachOption in cur_options:
+    #     tempstr=""
+    #     for item in eachOption:
+    #         tempstr+=item+"##"
+    #     cur_options_processed.append(tempstr)
     writer.writerow([cur_seral,cur_qus_name,cur_question_text, cur_Q_TABLE,cur_answer_format,cur_ans_col_header_l1,cur_ans_col_header_l2,cur_ans_row_header_l2,cur_options[0],cur_options[1],cur_options[2],cur_options[3]])
-
+#
 
 
 newCSVfile.close()
